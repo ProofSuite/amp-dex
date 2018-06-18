39637bd7ee615901f73fce3e63c7a4f0ccf0f7cb
Mukund Sonaiya <mukund.sonaiya@smartsensesolutions.com>
2018-06-18 16:39:14 +0530

    Add 'WETH token management' unit tests

--- test/exchange.js
+++ test/exchange.js
@@ -2,6 +2,7 @@
 import chai from 'chai'
 import Web3 from 'web3'
 import bnChai from 'bn-chai'
+import {expectRevert} from './helpers'
 
 chai
     .use(require('chai-bignumber')(web3.BigNumber))
@@ -10,11 +11,16 @@ chai
 
 const WETH = artifacts.require('./utils/WETH9.sol');
 const Exchange = artifacts.require('./Exchange.sol');
+const Token1 = artifacts.require('./contracts/tokens/Token1.sol');
+const Token2 = artifacts.require('./contracts/tokens/Token2.sol');
 
 contract('Exchange', (accounts) => {
+    let web3 = new Web3('http://localhost:8545');
 
     let owner = accounts[0];
     let feeAccount = accounts[1];
+    let operator = accounts[2];
+    let anyUser = accounts[5];
 
     let exchange;
     let weth;
@@ -38,6 +44,28 @@ contract('Exchange', (accounts) => {
         it('should initialise WETH token contract correctly', async () => {
             let initializedWethTokenContract = await exchange.WETH_TOKEN_CONTRACT.call();
             initializedWethTokenContract.should.be.equal(weth.address)
+        })
+    });
+
+    describe('WETH token management', async () => {
+        beforeEach(async () => {
+            weth = await WETH.new();
+            exchange = await Exchange.new(weth.address, feeAccount)
+        });
+
+        it('should set WETH token address if requested by owner', async () => {
+            let expectedWethTokenAddress = accounts[6];
+            await exchange.setWethToken(expectedWethTokenAddress, {from: owner});
+
+            let wethTokenContractAddress = await exchange.WETH_TOKEN_CONTRACT.call();
+            wethTokenContractAddress.should.be.equal(expectedWethTokenAddress)
+        });
+
+        it('should not set WETH token address if not requested by owner', async () => {
+            await exchange.setOperator(operator, true, {from: owner});
+            let newWethTokenAddress = accounts[6];
+            await expectRevert(exchange.setWethToken(newWethTokenAddress, {from: operator}));
+            await expectRevert(exchange.setWethToken(newWethTokenAddress, {from: anyUser}))
         })
     });
 
